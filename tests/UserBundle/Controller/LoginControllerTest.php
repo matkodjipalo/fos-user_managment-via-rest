0f46aa6ef924ff9c831533be51a254594a2eb488
Matko Djipalo <matkodjipalo@gmail.com>
2016-09-13 21:06:54 +0200

    - new test

--- tests/UserBundle/Controller/LoginControllerTest.php
+++ tests/UserBundle/Controller/LoginControllerTest.php
@@ -29,4 +29,28 @@ class LoginControllerTest extends ApiTestCaseBase
         $responseArr = json_decode($this->client->getResponse()->getContent(), true);
         $this->assertArrayHasKey('token', $responseArr);
     }
+
+    public function testPOSTLoginUserWithWrongUsername()
+    {
+        $userName = "mate.misho";
+        $password = "ja_sam_Dalmatino_1950";
+
+        $user = $this->createUser($userName, $password);
+
+        $this->client->request(
+            'POST',
+            '/users/login',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+                'PHP_AUTH_USER' => $userName.'wrong_username',
+                'PHP_AUTH_PW'   => $password,
+            ]
+        );
+
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+        $responseArr = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertEquals('Not Found', $responseArr['error']['message']);
+    }
 }
